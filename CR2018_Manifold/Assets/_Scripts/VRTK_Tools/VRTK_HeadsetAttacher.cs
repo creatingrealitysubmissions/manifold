c821c4be893675c2e7eece9d8db253698a928273
Michaelwolf95 <michaelwolf95@gmail.com>
2018-03-13 15:29:09 -0700

    SteamVR, VRTK, and some other work

---
+++ CR2018_Manifold/Assets/_Scripts/VRTK_Tools/VRTK_HeadsetAttacher.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using VRTK;
+
+namespace CR18_Manifold
+{
+    public class VRTK_HeadsetAttacher : MonoBehaviour
+    {
+        private bool _isAttached = false;
+        protected virtual void Awake()
+        {
+            VRTK_SDKManager.instance.AddBehaviourToToggleOnLoadedSetupChange(this);
+        }
+
+        protected virtual void OnEnable()
+        {
+            if (!_isAttached)
+            {
+                var headset = VRTK_DeviceFinder.HeadsetTransform();
+                if (headset)
+                {
+                    this.transform.SetParent(headset.transform);
+                    this.transform.localPosition = Vector3.zero;
+                    this.transform.localRotation = Quaternion.identity;
+                    _isAttached = true;
+                }
+            }
+        }
+
+        protected virtual void OnDisable()
+        {
+            
+        }
+
+        protected virtual void OnDestroy()
+        {
+            VRTK_SDKManager.instance.RemoveBehaviourToToggleOnLoadedSetupChange(this);
+        }
+    }
+}
