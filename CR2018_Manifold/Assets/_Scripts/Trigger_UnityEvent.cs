141b9f867c12b868e756e82e52181c3cf94806fd
Michaelwolf95 <michaelwolf95@gmail.com>
2018-03-13 11:31:20 -0700

    Imported Standard Assets characters and began working on manifold system

---
+++ CR2018_Manifold/Assets/_Scripts/Trigger_UnityEvent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CR18_Manifold
+{
+    public class Trigger_UnityEvent : MonoBehaviour
+    {
+        public UnityEvent OnEnterEvent;
+        public UnityEvent OnExitEvent;
+
+        public string TriggerTag = "Player";
+        private GameObject cachedGameObject;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (cachedGameObject) return;
+            if (other.CompareTag(TriggerTag))
+            {
+                cachedGameObject = other.attachedRigidbody ? other.attachedRigidbody.gameObject : other.gameObject;
+                OnEnterEvent.Invoke();
+            }
+
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (!cachedGameObject) return;
+            if (other.CompareTag(TriggerTag))
+            {
+                var go = other.attachedRigidbody ? other.attachedRigidbody.gameObject : other.gameObject;
+                if (go == cachedGameObject)
+                {
+                    OnEnterEvent.Invoke();
+                    cachedGameObject = null;
+                }
+            }
+        }
+    }
+
+}
+
