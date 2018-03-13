c161b596abd16b88beedb258389de84a3c3040df
Michaelwolf95 <michaelwolf95@gmail.com>
2018-03-13 13:40:08 -0700

    Created Manifold Portal

---
+++ CR2018_Manifold/Assets/_Scripts/ManifoldRoomController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace CR18_Manifold
+{
+    public class ManifoldRoomController : MonoBehaviour
+    {
+        public bool IsEnabled = false;
+        public UnityEvent OnEnableRoom;
+        public UnityEvent OnDisableRoom;
+
+        void Start()
+        {
+            SetRoomEnabled(IsEnabled);
+        }
+
+        public void EnableRoom()
+        {
+            if (IsEnabled) return;
+            SetRoomEnabled(true);
+        }
+
+        public void DisableRoom()
+        {
+            if(!IsEnabled) return;
+            SetRoomEnabled(false);
+        }
+
+        public void ToggleRoom()
+        {
+            SetRoomEnabled(!IsEnabled);
+        }
+
+        public void SetRoomEnabled(bool state)
+        {
+            
+            IsEnabled = state;
+            if (IsEnabled)
+            {
+                OnEnableRoom.Invoke();
+            }
+            else
+            {
+                OnDisableRoom.Invoke();
+            }
+        }
+    }
+}
