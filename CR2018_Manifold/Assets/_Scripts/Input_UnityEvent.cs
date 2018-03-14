bc4b715dc3eb4ee7702f3c1c6b22be772a820b47
Michaelwolf95 <michaelwolf95@gmail.com>
2018-03-14 13:42:34 -0700

    Integrated full blockout with audio system.

---
+++ CR2018_Manifold/Assets/_Scripts/Input_UnityEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Input_UnityEvent : MonoBehaviour
+{
+    public KeyCode Key;
+    public UnityEvent InputEvent;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Key))
+        {
+            InputEvent.Invoke();
+        }
+    }
+}
---
+++ CR2018_Manifold/Assets/_Scripts/Timed_UnityEvent.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Timed_UnityEvent : MonoBehaviour
+{
+    public float Duration = 1f;
+    public bool StartOnEnable = false;
+    public UnityEvent TimerEvent;
+
+    private Coroutine _coroutine;
+
+    private void OnEnable()
+    {
+        if (StartOnEnable)
+        {
+            StartTimer();
+        }
+    }
+
+    private void OnDisable()
+    {
+        StopTimer();
+    }
+
+    public void StartTimer()
+    {
+        if(_coroutine != null) StopTimer();
+        Debug.Log("Starting Timer");
+        _coroutine = StartCoroutine(CoEventTimer(Duration));
+    }
+
+    public void StopTimer()
+    {
+        if (_coroutine != null)
+        {
+            Debug.Log("Stopping Timer");
+            StopCoroutine(_coroutine);
+            _coroutine = null;
+        }
+    }
+
+    private IEnumerator CoEventTimer(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        TimerEvent.Invoke();
+        _coroutine = null;
+    }
+}
