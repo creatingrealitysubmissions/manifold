f12dc495c4cdc6a8a2768895955e63e339076026
Michaelwolf95 <michaelwolf95@gmail.com>
2018-03-14 16:47:07 -0700

    Added 360 video control system

---
+++ CR2018_Manifold/Assets/_Scripts/ScreenFadeController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class ScreenFadeController : MonoBehaviour
+{
+    public void ScreenFadeOut(float duration)
+    {
+        VRTK.VRTK_ScreenFade.Start(Color.black, duration);
+    }
+    public void ScreenFadeIn(float duration)
+    {
+        VRTK.VRTK_ScreenFade.Start(Color.clear, duration);
+    }
+}
