eef832d64e49b291aa3ed2ed02fbb4c5621991e9
Michaelwolf95 <michaelwolf95@gmail.com>
2018-03-14 18:25:30 -0700

    Fixed end screenfade

--- CR2018_Manifold/Assets/_Scripts/ScreenFadeController.cs
+++ CR2018_Manifold/Assets/_Scripts/ScreenFadeController.cs
@@ -6,10 +6,13 @@ public class ScreenFadeController : MonoBehaviour
 {
     public void ScreenFadeOut(float duration)
     {
-        VRTK.VRTK_ScreenFade.Start(Color.black, duration);
+        //Debug.Log("Fade Out");
+        FindObjectOfType<VRTK_HeadsetFade>().Fade(Color.black, duration);
+        //VRTK.VRTK_ScreenFade.Start(Color.black, duration);
     }
     public void ScreenFadeIn(float duration)
     {
-        VRTK.VRTK_ScreenFade.Start(Color.clear, duration);
+        //VRTK.VRTK_ScreenFade.Start(Color.clear, duration);
+        FindObjectOfType<VRTK_HeadsetFade>().Fade(Color.clear, duration);
     }
 }
